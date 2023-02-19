fe18462885010149bee157cf170965ccaeb4af34
Nikolay Natov <64912680+NikiNatov@users.noreply.github.com>
2023-02-19 02:00:23 +0200

    Leave VSync enabled by default for the editor app

--- AtomEditor/src/EditorApplication.cpp
+++ AtomEditor/src/EditorApplication.cpp
@@ -26,7 +26,6 @@ namespace Atom
         ApplicationSpecification spec;
         spec.Name = "Atom Editor";
         spec.CommandLineArgs = args;
-        spec.VSync = false;
         spec.AppLoggerSinks = {
             { std::make_shared<spdlog::sinks::stdout_color_sink_mt>(), "%^[%T] %n: %v%$" },
             { std::make_shared<ConsoleSink>(), "%^[%T] [%l]: %v%$" }
