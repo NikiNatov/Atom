69d75c4049dd3157f9ecce56330e5c9cf2e74f0e
Nikolay Natov <64912680+NikiNatov@users.noreply.github.com>
2023-12-18 00:18:49 +0200

    Added fps counter

--- AtomEditor/src/EditorLayer.cpp
+++ AtomEditor/src/EditorLayer.cpp
@@ -248,6 +248,25 @@ namespace Atom
         const Texture* finalImage = m_Renderer->GetFinalImage();
         ImGui::Image((ImTextureID)finalImage, {(f32)finalImage->GetWidth(), (f32)finalImage->GetHeight()});
 
+        ImVec2 fpsTextPos = prevPos;
+        fpsTextPos.x += 5.0f;
+        fpsTextPos.y += 5.0f;
+
+        u32 fps = Application::Get().GetFPS();
+        ImVec4 fpsTextColor;
+
+        if (fps < 20)
+            fpsTextColor = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+        else if(fps >= 20 && fps < 30)
+            fpsTextColor = ImVec4(1.0f, 0.8f, 0.0f, 1.0f);
+        else
+            fpsTextColor = ImVec4(0.0f, 1.0f, 0.0f, 1.0f);
+
+        ImGui::SetCursorPos(fpsTextPos);
+        ImGui::PushStyleColor(ImGuiCol_Text, fpsTextColor);
+        ImGui::Text("FPS: %d", fps);
+        ImGui::PopStyleColor();
+
         if (m_ActiveScene->GetSceneState() == SceneState::Running)
         {
             ImGui::SetCursorPos(prevPos);
--- AtomRuntime/src/RuntimeLayer.cpp
+++ AtomRuntime/src/RuntimeLayer.cpp
@@ -57,6 +57,29 @@ namespace Atom
         ImGui::SetNextWindowPos(ImVec2{ 0.0f, 0.0f });
         ImGui::SetNextWindowSize(ImVec2{ (f32)window.GetWidth(), (f32)window.GetHeight() });
         ImGui::Begin("GameUI", 0, ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
+
+        ImVec2 startPos = ImGui::GetCursorPos();
+
+        ImVec2 fpsTextPos = startPos;
+        fpsTextPos.x += 5.0f;
+        fpsTextPos.y += 5.0f;
+
+        u32 fps = Application::Get().GetFPS();
+        ImVec4 fpsTextColor;
+
+        if (fps < 20)
+            fpsTextColor = ImVec4(1.0f, 0.0f, 0.0f, 1.0f);
+        else if (fps >= 20 && fps < 30)
+            fpsTextColor = ImVec4(1.0f, 0.8f, 0.0f, 1.0f);
+        else
+            fpsTextColor = ImVec4(0.0f, 1.0f, 0.0f, 1.0f);
+
+        ImGui::SetCursorPos(fpsTextPos);
+        ImGui::PushStyleColor(ImGuiCol_Text, fpsTextColor);
+        ImGui::Text("FPS: %d", fps);
+        ImGui::PopStyleColor();
+
+        ImGui::SetCursorPos(startPos);
         m_ActiveScene->OnImGuiRender();
         ImGui::End();
         ImGui::PopStyleVar();
